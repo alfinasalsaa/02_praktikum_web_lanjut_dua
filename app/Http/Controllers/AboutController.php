72f5a4f670f37a3cf4bb9dd7f654e50a8fbf8f76
alfinasalsa <alfinasalsabilla51@gmail.com>
2023-02-21 16:59:28 +0700

    first commit

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    //
+    public function index(){
+        return 'selamat datang';
+    }
+
+     public function about(){
+        return '2141720044 --- Alfina Salsabilla';
+    }
+
+     public function articles($id){
+        return "halaman artikel dengan ID ($id)";
+    }
+}
